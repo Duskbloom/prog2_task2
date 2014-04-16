c91172dc7bd7e612c81951cbe97d86e9b6b55c24
Sofie Andersson <sofie1.andersson@gmail.com>
2014-04-16 10:15:07 +0200

    made jlabel into a drawString

--- src/Marker.java
+++ src/Marker.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -5,19 +6,19 @@ import java.awt.Graphics;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Marker<T> extends JPanel{
   private T item;
   private int y;
   private int x;
-  private JLabel namn;
+  private JLabel name;
+  private boolean active = false;
     
   Marker(int x, int y, T item){
     this.x = x-25;
     this.y = y-15;
-    this.item = item; 
-    namn = new JLabel(item.toString());
-    add(namn);
+    this.item = item;
     Dimension d = new Dimension(50,50);
     setPreferredSize(d);
     setLocation(x, y);
@@ -37,9 +38,22 @@ public class Marker<T> extends JPanel{
   public T getItem(){
     return item;
   }
-
+  public boolean isActive() {
+    return active;
+  }
+  public void setActive(boolean active) {
+    this.active = active;
+  }
   protected void paintComponent(Graphics g){
-    g.setColor(Color.CYAN);
-    g.fillOval(10, 0, 30, 30);
+    if(!active){
+      g.setColor(Color.CYAN);
+      g.fillOval(10, 0, 30, 30);
+    }
+    else{
+      g.setColor(Color.RED);
+      g.fillOval(10, 0, 30, 30);
+    }
+    g.setColor(Color.BLACK);
+    g.drawString(item.toString(), 10, 45);
   }
 }
